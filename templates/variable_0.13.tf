f15db845a8c4c73d472a884a9b5e3ece68c1268c
ROMAIN ECARNOT <romain.ecarnot@open-groupe.com>
2020-08-18 10:23:43 +0200

    remove variable validation block. Needs fix

--- templates/variable_0.13.tf
+++ templates/variable_0.13.tf
@@ -3,8 +3,10 @@ variable "{{name}}" {
   description = "{{description}}"
   type        = {{type}}
   
+  /*
   validation {
     condition     = "{{condition}}"
     error_message = "{{error_message}}"
   }
+  */
 }
